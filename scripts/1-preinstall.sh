4b06bd45f070174f366d950c70fb9bc9d4be45ab
Melchizedek Shah <melchizedekshah18@proton.me>
2025-09-25 14:07:08 -0400

    update cmdline linux default grub

--- scripts/1-preinstall.sh
+++ scripts/1-preinstall.sh
@@ -34,7 +34,7 @@ if [[ $DISK_ENCRYPT = 'y' ]]; then
     sed -i 's/^#\?GRUB_ENABLE_CRYPTODISK=.*/GRUB_ENABLE_CRYPTODISK=y/' /etc/default/grub
 
     # 2. Append to GRUB_CMDLINE_LINUX_DEFAULT instead of overwriting
-    sed -i "s|^GRUB_CMDLINE_LINUX_DEFAULT=\"\(.*\)\"|GRUB_CMDLINE_LINUX_DEFAULT=\"\1 cryptdevice=UUID=${LUKS_UUID}:cryptlvm root=UUID=${ROOT_UUID}\"|" /etc/default/grub
+    sed -i "s|^GRUB_CMDLINE_LINUX_DEFAULT=\"\(.*\)\"|GRUB_CMDLINE_LINUX_DEFAULT=\"\1 cryptdevice=UUID=${LUKS_UUID}:cryptlvm root=/dev/archvolume/root\"|" /etc/default/grub
 fi
 
 grub-mkconfig -o /boot/grub/grub.cfg
