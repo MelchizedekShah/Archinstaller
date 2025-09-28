2318abe9db1d3849d129dee79bcb493cdc72fbdb
Melchizedek Shah <melchizedekshah18@proton.me>
2025-09-28 13:58:03 -0400

    Update 3-user.sh

--- scripts/3-user.sh
+++ scripts/3-user.sh
@@ -14,20 +14,20 @@ if [[ $de_choice == "XFCE" ]]; then
     echo "XFCE setup..."
     sudo pacman -S --needed --noconfirm xorg-xinit
 
-    # edit the xinitrc file
-    cat > /home/${username}/.xinitrc << "EOF"
-    #!/bin/bash
-    exec startxfce4
-    EOF
+    # overwrite the xinitrc file
+    cat > /home/${username}/.xinitrc << EOF
+#!/bin/bash
+exec startxfce4
+EOF
     chmod +x /home/${username}/.xinitrc
 
-    # edit the bash profile file
+    # append to bash_profile if not already present
     if ! grep -q "exec startx" /home/${username}/.bash_profile 2>/dev/null; then
-    cat >> /home/${username}/.bash_profile << "EOF"
-    if [[ -z $DISPLAY ]] && [[ $(tty) == /dev/tty1 ]]; then
-        exec startx
-    fi
-    EOF
+        cat >> /home/${username}/.bash_profile << EOF
+if [[ -z \$DISPLAY ]] && [[ \$(tty) == /dev/tty1 ]]; then
+    exec startx
+fi
+EOF
     fi
 fi
 
