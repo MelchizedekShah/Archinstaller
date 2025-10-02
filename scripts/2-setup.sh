b71835fbe312b53b0e8ed55f85f1cdbdb25d2d9f
Melchizedek Shah <melchizedekshah18@proton.me>
2025-10-02 14:40:41 -0400

    Update 2-setup.sh

--- scripts/2-setup.sh
+++ scripts/2-setup.sh
@@ -1,16 +1,17 @@
+
 #!/bin/bash
 
-# funtion with all the variables that are handed over from 0-preinstall.sh
+# function with all the variables that are handed over from 0-preinstall.sh
 source /usr/local/share/Archinstaller/vars.sh
 
-
 installpackage() {
+    local pkgs="$@"
     while true; do
-        if ! pacman -S --noconfirm --needed ${packages}; then
-            echo "ERROR: Package installation failed"
-            echo "Try again.."
+        if ! pacman -S --noconfirm --needed $pkgs; then
+            echo "ERROR: Failed to install: $pkgs"
+            echo "Retrying..."
         else
-            echo "Succes"
+            echo "SUCCESS: Installed $pkgs"
             break
         fi
     done
@@ -30,16 +31,14 @@ echo -ne "
                             ucode
 -------------------------------------------------------------------------
 "
-# The mkinitcpio is done here
-
 cpu_type=$(lscpu)
 if grep -E "GenuineIntel" <<< ${cpu_type}; then
     echo "Installing Intel microcode"
-    pacman -S --noconfirm --needed intel-ucode
+    installpackage intel-ucode
     proc_ucode=intel-ucode.img
 elif grep -E "AuthenticAMD" <<< ${cpu_type}; then
     echo "Installing AMD microcode"
-    pacman -S --noconfirm --needed amd-ucode
+    installpackage amd-ucode
     proc_ucode=amd-ucode.img
 fi
 
@@ -49,27 +48,24 @@ echo -ne "
 -------------------------------------------------------------------------
 "
 
-# In the chrooted script - check user's previous choice
 if [[ $nvidia_install == "y" ]]; then
     echo "Installing NVIDIA proprietary drivers..."
-    # Add NVIDIA driver installation here
-    pacman -S --noconfirm --needed nvidia-dkms nvidia-utils linux-headers linux-lts-headers
+    installpackage nvidia-dkms nvidia-utils linux-headers linux-lts-headers
 elif grep -E "NVIDIA|GeForce" <<< $(lspci) && [[ $nvidia_install == "n" ]]; then
     echo "NVIDIA detected but user chose to skip - installing fallback drivers..."
-    # For dual graphics, install drivers for the OTHER GPU
     if lspci | grep 'VGA' | grep -E "Radeon|AMD"; then
         echo "Installing AMD drivers for dual graphics setup..."
-        pacman -S --noconfirm --needed vulkan-radeon mesa
+        installpackage vulkan-radeon mesa
     elif lspci | grep 'VGA' | grep -E "Intel"; then
         echo "Installing Intel drivers for dual graphics setup..."
-        pacman -S --noconfirm --needed vulkan-intel mesa
+        installpackage vulkan-intel mesa
     fi
 elif lspci | grep 'VGA' | grep -E "Radeon|AMD"; then
     echo "AMD GPU detected..."
-    pacman -S --noconfirm --needed vulkan-radeon mesa
+    installpackage vulkan-radeon mesa
 elif lspci | grep 'VGA' | grep -E "Intel"; then
     echo "Intel GPU detected..."
-    pacman -S --noconfirm --needed vulkan-intel mesa
+    installpackage vulkan-intel mesa
 else
     echo "No recognized GPU found"
 fi
@@ -83,26 +79,22 @@ echo -ne "
 case $de_choice in
     KDE)
         echo "Installing KDE Plasma..."
-        packages="plasma-meta sddm"
-        installpackage
+        installpackage plasma-meta sddm
         systemctl enable sddm
         echo "KDE Plasma installed successfully!"
         ;;
     GNOME)
-         echo "Installing GNOME..."
-         packages="gnome gdm"
-         installpackage
-         systemctl enable gdm
-         echo "GNOME installed successfully!"
-         ;;
+        echo "Installing GNOME..."
+        installpackage gnome gdm
+        systemctl enable gdm
+        echo "GNOME installed successfully!"
+        ;;
     XFCE)
         echo "Installing XFCE..."
-        packages="xfce4 xfce4-goodies xorg \
-        pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber \
-        xfce4-pulseaudio-plugin pavucontrol \
-        gst-plugin-pipewire alsa-utils network-manager-applet"
-
-        installpackage
+        installpackage xfce4 xfce4-goodies xorg \
+            pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber \
+            xfce4-pulseaudio-plugin pavucontrol \
+            gst-plugin-pipewire alsa-utils network-manager-applet
         systemctl enable sddm
         echo "XFCE installed successfully!"
         ;;
@@ -110,12 +102,6 @@ case $de_choice in
         echo "Server or minimum setup"
         echo "Not installing DE"
         ;;
-
-    esac
-
-
-
-
-
+esac
 
 echo "Finished 2-setup.sh"
